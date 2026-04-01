fa7fef3baa57f9fad6c44f8f8f049c7a36c559e7
Lewin Kelly <heli@odex.cf>
2026-04-01 20:27:25 +0100

    Track time to parse UFO and compile to OTF

--- python/main.py
+++ python/main.py
@@ -1,8 +1,15 @@
+import time
+
 from defcon import Font
 from ufo2ft import compileOTF
 
 print("hello, world")
 
+# time
+start = time.time()
+
 ufo = Font("main.ufo")
 otf = compileOTF(ufo)
 otf.save("out.otf")
+end = time.time()
+print(f"Time taken: {(end - start) * 1000}ms")
